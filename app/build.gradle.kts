a3ef7042bd437409017f1eec7d28bfbaa1a735c3
Toby Inkster <github@toby.ink>
2026-05-29 01:01:55 +0100

    bump version, update js

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "org.zuzulang.repl"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 1
-		versionName = "0.1.0"
+		versionCode = 2
+		versionName = "0.1.1"
 
 		testInstrumentationRunner =
 			"androidx.test.runner.AndroidJUnitRunner"
