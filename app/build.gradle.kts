a2c6f9cfc88a152634f10230b78bf2be63841ab4
Toby Inkster <github@toby.ink>
2026-06-10 01:02:19 +0100

    Bump Android app and zuzu-js to 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "org.zuzulang.repl"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 2
-		versionName = "0.1.1"
+		versionCode = 3
+		versionName = "0.3.0"
 
 		testInstrumentationRunner =
 			"androidx.test.runner.AndroidJUnitRunner"
