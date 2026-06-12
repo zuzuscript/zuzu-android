8393fef2b19f1a1a05d893c00d109c00635c9ad0
Toby Inkster <github@toby.ink>
2026-06-12 17:09:51 +0100

    Release Android 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "org.zuzulang.repl"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 3
-		versionName = "0.3.0"
+		versionCode = 4
+		versionName = "0.4.0"
 
 		testInstrumentationRunner =
 			"androidx.test.runner.AndroidJUnitRunner"
