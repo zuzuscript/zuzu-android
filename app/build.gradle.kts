8d2a12cc1242cee23b3bae711495212e86747272
Toby Inkster <github@toby.ink>
2026-06-25 17:36:00 +0100

    Release 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 		applicationId = "org.zuzulang.repl"
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 6
-		versionName = "0.6.0"
+		versionCode = 7
+		versionName = "0.7.0"
 
 		testInstrumentationRunner =
 			"androidx.test.runner.AndroidJUnitRunner"
